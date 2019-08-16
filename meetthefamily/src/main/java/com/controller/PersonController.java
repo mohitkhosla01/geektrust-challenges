91b2caaf728373d44c2cfc7a1b23d021b835ef92
mohitkhosla01 <mohit.khosla.01@gmail.com>
2019-08-16 09:40:10 +0530

    Creating Geektrust challenges repo. Adding initial files of 'Meet the Family' challenge

---
+++ meetthefamily/src/main/java/com/controller/PersonController.java
@@ -0,0 +1,15 @@
+package com.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.dto.Person;
+
+public class PersonController {
+	
+	protected Map<String, Person> familyMembers = new HashMap<String, Person>();
+
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ meetthefamily/src/main/java/com/dto/Person.java
@@ -0,0 +1,34 @@
+package com.dto;
+
+public class Person {
+	
+	private String name;
+	private String gender;
+	private String motherName;
+	private String siblingName;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getMotherName() {
+		return motherName;
+	}
+	public void setMotherName(String motherName) {
+		this.motherName = motherName;
+	}
+	public String getSiblingName() {
+		return siblingName;
+	}
+	public void setSiblingName(String siblingName) {
+		this.siblingName = siblingName;
+	}
+}
