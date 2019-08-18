50edf66fabb83648c2a6b9aaccf840d840d71878
mohitkhosla01 <mohit.khosla.01@gmail.com>
2019-08-18 20:55:41 +0530

    Modified 'MeetTheFamily' to accept input from files

--- meetthefamily/src/main/java/com/controller/FamilyTreeController.java
+++ meetthefamily/src/main/java/com/controller/FamilyTreeController.java
@@ -1,8 +1,13 @@
 package com.controller;
 
-import java.util.Map;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 
 import com.dto.Person;
 import com.service.FamilyTreeCreateService;
@@ -19,57 +24,71 @@ public class FamilyTreeController {
 		 * VALUE: Person object references
 		 */
 
-		FamilyTreeCreateService.constructInitialFamilyTree(familyMembers);
-
-		String[] inputs = {		    
-				"GET_RELATIONSHIP Satvy Sister-In-Law",
-				"GET_RELATIONSHIP Ish Son",
-				"GET_RELATIONSHIP Misha Daughter"
-		};
-
 		
-		for(String input : inputs) {
+		FamilyTreeCreateService.constructInitialFamilyTree(familyMembers);
+		
+		
+		Scanner sc = new Scanner(System.in);
+		String fileSource = sc.nextLine();
+		
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(fileSource));
+		    
+			String line;
+		    while ((line = br.readLine()) != null) {
+		       
+				String[] inputParameters = line.split(" ");
 
-			String[] inputParameters = input.split(" ");
+				if(inputParameters[0].equals("ADD_CHILD")) {
 
-			if(inputParameters[0].equals("ADD_CHILD")) {
+					if(inputParameters.length == 4) {
+						String mothersName = inputParameters[1];
+						String childsName = inputParameters[2];
+						String childsGender = inputParameters[3];
 
-				if(inputParameters.length == 4) {
-					String mothersName = inputParameters[1];
-					String childsName = inputParameters[2];
-					String childsGender = inputParameters[3];
-
-					System.out.println(FamilyTreeManageService.addChild(familyMembers, mothersName, childsName, childsGender));
-				}
-				else {
-					System.out.println("INVALID_INPUT");
-				}
-			}
-			else if(inputParameters[0].equals("GET_RELATIONSHIP")) {
-
-				if(inputParameters.length == 3) {
-					String person = inputParameters[1];
-					String relationship = inputParameters[2];
-					
-					List<String> relatives = FamilyTreeManageService.getRelatives(familyMembers, person, relationship);
-					
-					if(relatives.size() > 0) {
-						for(String relative : relatives) {
-							System.out.print(relative + " ");
-						}
-						System.out.println();
+						System.out.println(FamilyTreeManageService.addChild(familyMembers, mothersName, childsName, childsGender));
 					}
 					else {
-						System.out.println("NONE");
+						System.out.println("INVALID_INPUT");
+					}
+				}
+				else if(inputParameters[0].equals("GET_RELATIONSHIP")) {
+
+					if(inputParameters.length == 3) {
+						String person = inputParameters[1];
+						String relationship = inputParameters[2];
+						
+						List<String> relatives = FamilyTreeManageService.getRelatives(familyMembers, person, relationship);
+						
+						if(relatives.size() > 0) {
+							for(String relative : relatives) {
+								System.out.print(relative + " ");
+							}
+							System.out.println();
+						}
+						else {
+							System.out.println("NONE");
+						}
+					}
+					else {
+						System.out.println("INVALID_INPUT");
 					}
 				}
 				else {
 					System.out.println("INVALID_INPUT");
 				}
-			}
-			else {
-				System.out.println("INVALID_INPUT");
-			}
+		    }
+		    
+		    br.close();
+		} 
+		catch(FileNotFoundException fne) {
+			System.out.println("No file found at location: " + fileSource);
 		}
+		catch(IOException ioe) {
+			System.out.println("IOException occured!");
+			ioe.printStackTrace();
+		}
+		
+		sc.close();
 	}
 }
